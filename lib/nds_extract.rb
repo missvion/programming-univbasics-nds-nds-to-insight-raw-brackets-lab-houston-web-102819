2570f09e06b577b7ddca4651b3ac9f3f43c78777
Vivian Tran <viviantran94@gmail.com>
2019-10-22 22:47:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,3 +24,20 @@ def directors_totals(nds)
 
   total
 end
+
+
+grand_total = {}
+row_index = 0
+while row_index < nds.length do
+  movie_index = 0
+  while movie_index < nds[row_index][:movies].length do
+    inner_len = vm[row_index][column_index].length
+    inner_index = 0
+    while inner_index < inner_len do
+      grand_total += nds[row_index][:movie][movie_index][:worldwide_gross]
+      inner_index += 1
+    end
+    movie_index += 1
+  end
+  row_index += 1
+end
