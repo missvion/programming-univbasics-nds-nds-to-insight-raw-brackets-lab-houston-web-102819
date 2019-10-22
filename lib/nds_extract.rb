10fea24e728df64800ce3c174bca4960d8f7945a
Vivian Tran <viviantran94@gmail.com>
2019-10-22 22:48:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,25 +6,6 @@ pp directors_database
   
 #------------------------------------------
 def directors_totals(nds)
-  director_index = 0
-  total = {}
-
-  while director_index < nds.length do
-    name_index = nds[director_index][:name]
-    total[name_index] = 0
-    movie_index = 0
-
-    while movie_index < nds[director_index][:movies].length do
-      total[name_index] += nds[director_index][:movies][movie_index][:worldwide_gross]
-      movie_index += 1
-    end
-
-    director_index += 1
-  end
-
-  total
-end
-
 
 grand_total = {}
 row_index = 0
